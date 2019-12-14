fcd5153e6c804463f28113ed87d4c2fee2145f69
WJ-Lai <418320384@qq.com>
2019-12-14 09:43:26 +0800

    get_all_nms_results

--- src/test_nms.py
+++ src/test_nms.py
@@ -22,6 +22,21 @@ from external.nms import soft_nms
 from itertools import combinations
 import copy
 
+import sys
+
+
+class Logger_txt(object):
+  def __init__(self, fileN="Default.log"):
+    self.terminal = sys.stdout
+    self.log = open(fileN, "a")
+
+  def write(self, message):
+    self.terminal.write(message)
+    self.log.write(message)
+
+  def flush(self):
+    pass
+
 def test(opt):
   os.environ['CUDA_VISIBLE_DEVICES'] = opt.gpus_str
 
@@ -108,9 +123,10 @@ def load_result():
   dataset = Dataset(opt, 'test')
   return results_rgb, results_fir, results_mir, results_nir, dataset
 
-def output_result(opt, dataset):
+def output_result(opt, dataset, model_type):
+  assert model_type in [333,666,'best','last'], 'Model type must in [333,666,\'best\',\'last\']'
   opt.dataset = dataset
-  opt.load_model = '/home/vincent/Checkpoint/CenterNet-CentralNet/ctdet/single2/'+dataset+'/model_best.pth'
+  opt.load_model = '/home/vincent/Checkpoint/CenterNet-CentralNet/ctdet/single2/'+dataset+'/model_'+str(model_type)+'.pth'
   results, dataset = test(opt)
   return results, dataset
 
@@ -148,12 +164,13 @@ def get_all_sensor_combinations():
           sensor_combinations.append(list(j))
   return sensor_combinations
 
-if __name__ == '__main__':
-  opt = opts().parse()
-  results_rgb, dataset = output_result(opt, 'rgb')
-  results_fir, dataset = output_result(opt, 'fir')
-  results_mir, dataset = output_result(opt, 'mir')
-  results_nir, dataset = output_result(opt, 'nir')
+
+def output_all_results(model_type):
+  print(model_type)
+  results_rgb, dataset = output_result(opt, 'rgb', model_type)
+  results_fir, dataset = output_result(opt, 'fir', model_type)
+  results_mir, dataset = output_result(opt, 'mir', model_type)
+  results_nir, dataset = output_result(opt, 'nir', model_type)
   save_result(results_rgb, results_fir, results_mir, results_nir)
 
   # output_sing_mAP()
@@ -170,3 +187,16 @@ if __name__ == '__main__':
     results = nms_multi(results)
     print(sensor_list)
     cal_mAP(results, dataset)
+
+
+if __name__ == '__main__':
+  sys.stdout = Logger_txt("results.txt")
+  opt = opts().parse()
+  for opt.test_dataset in ['test', 'val']:
+    print(opt.test_dataset)
+    for model_type in [333,666,'best','last']:
+      output_all_results(model_type)
+      print('')
+      print('')
+    print('')
+    print('')
